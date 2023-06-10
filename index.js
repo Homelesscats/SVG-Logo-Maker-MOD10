285aa33b77fe2a1d903e4313a0965d124eea5e59
homelesscat <gilbertperezdt@gmail.com>
2023-06-09 18:03:54 -0700

    array of question added to index.js

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+
+
+const questions = 
+
+
+[
+  {
+  type: 'input',
+  name: 'text',
+  message: 'Please enter text',    // This will need to be set enter up to 3 characters 
+  },
+
+  {
+  type: 'input',
+  name: 'text',
+  message: 'Please enter a text color', // Be sure to allow keyword or hexadecimal number 
+  }, 
+
+  {
+   type: 'list',
+   name: 'shape',
+   message: 'Please select a shape',
+   choices: 'circle, triangle, square',
+  },
+
+  {
+   type: 'input',
+   name: 'name',
+   message:'Please enter a color for the shape', // Be sure to allow keyword or hexadecimal number 
+  },
+
+
+];
+
+
