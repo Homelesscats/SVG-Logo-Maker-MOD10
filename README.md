bc89151cc90f4cb30e06b2a4d4c2ccda767538c9
homelesscat <gilbertperezdt@gmail.com>
2023-09-20 16:55:43 -0700

    index function create logo

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
 # SVG-Logo-Maker-MOD10
 
-## Description 
+AS a freelance web developer I NEED to generate a simple logo for my projects SO THAT I don't have to pay a graphic designer
 
+## Description
 
-## Acceptance Critera 
+Your task is to build a Node.js command-line application that takes in user input to generate a logo and save it as an SVG fileLinks to an external site.. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a .svg file.
 
+## Acceptance Critera
+
+GIVEN a command-line application that accepts user input  
+WHEN I am prompted for text  
+THEN I can enter up to three characters  
+WHEN I am prompted for the text color  
+THEN I can enter a color keyword (OR a hexadecimal number)  
+WHEN I am prompted for a shape  
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square  
+WHEN I am prompted for the shape's color  
+THEN I can enter a color keyword (OR a hexadecimal number)  
+WHEN I have entered input for all the prompts  
+THEN an SVG file is created named `logo.svg`  
+AND the output text "Generated logo.svg" is printed in the command line  
+WHEN I open the `logo.svg` file in a browser  
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
 ## Usage
 
+Because this application won’t be deployed, please see the video attached to see a demonstration of the logo maker
 
+## Resources
 
-## Resources 
+-w3schools
+
+-mdndocs
+
+-class content
 
 ## Deployed site:
-
--- index.js
+++ index.js
@@ -1,82 +1,295 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { setShape } = require("./lib/shapes.js");
 
-const fs = require(/node_modules/);
-const inquirer = require('inquirer');
-const { Circle, Square, Triangle } = require('./shape');
+const colorKeywordChoices = [
+  "aliceblue",
+  "antiquewhite",
+  "aqua",
+  "aquaMarine",
+  "azure",
+  "beige",
+  "bisque",
+  "black",
+  "blanchedalmond",
+  "blue",
+  "blueviolet",
+  "brown",
+  "burlywood",
+  "cadetblue",
+  "chartreuse",
+  "chocolate",
+  "coral",
+  "cornflowerblue",
+  "cornsilk",
+  "crimson",
+  "cyan",
+  "darkblue",
+  "darkcyan",
+  "darkgoldenrod",
+  "darkgray",
+  "darkgrey",
+  "darkgreen",
+  "darkkhaki",
+  "darkmagenta",
+  "darkolivegreen",
+  "darkorange",
+  "darkorchid",
+  "darkred",
+  "darksalmon",
+  "darkseagreen",
+  "darkslateblue",
+  "darkslategray",
+  "darkslategrey",
+  "darkturquoise",
+  "darkviolet",
+  "deeppink",
+  "deepskyblue",
+  "dimgray",
+  "dimgrey",
+  "dodgerblue",
+  "firebrick",
+  "floralwhite",
+  "forestgreen",
+  "fuchsia",
+  "gainsboro",
+  "ghostwhite",
+  "gold",
+  "goldenrod",
+  "gray",
+  "grey",
+  "green",
+  "greenyellow",
+  "honeydew",
+  "hotpink",
+  "indianred",
+  "indigo",
+  "ivory",
+  "khaki",
+  "lavender",
+  "lavenderblush",
+  "lawngreen",
+  "lemonchiffon",
+  "lightblue",
+  "lightcoral",
+  "lightcyan",
+  "lightgoldenrodyellow",
+  "lightgray",
+  "lightgrey",
+  "lightgreen",
+  "lightpink",
+  "lightsalmon",
+  "lightseagreen",
+  "lightskyblue",
+  "lightslategray",
+  "lightslategrey",
+  "lightsteelblue",
+  "lightyellow",
+  "lime",
+  "limegreen",
+  "linen",
+  "magenta",
+  "maroon",
+  "mediumaquamarine",
+  "mediumblue",
+  "mediumorchid",
+  "mediumpurple",
+  "mediumseagreen",
+  "mediumslateblue",
+  "mediumspringgreen",
+  "mediumturquoise",
+  "mediumvioletred",
+  "midnightblue",
+  "mintcream",
+  "mistyrose",
+  "moccasin",
+  "navajowhite",
+  "navy",
+  "oldlace",
+  "olive",
+  "olivedrab",
+  "orange",
+  "orangered",
+  "orchid",
+  "palegoldenrod",
+  "palegreen",
+  "paleturquoise",
+  "palevioletred",
+  "papayawhip",
+  "peachpuff",
+  "peru",
+  "pink",
+  "plum",
+  "powderblue",
+  "purple",
+  "rebeccapurple",
+  "red",
+  "rosybrown",
+  "royalblue",
+  "saddlebrown",
+  "salmon",
+  "sandybrown",
+  "seagreen",
+  "seashell",
+  "sienna",
+  "silver",
+  "skyblue",
+  "slateblue",
+  "slategray",
+  "slategrey",
+  "snow",
+  "springgreen",
+  "steelblue",
+  "tan",
+  "teal",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "wheat",
+  "white",
+  "whitesmoke",
+  "yellow",
+  "yellowgreen",
+];
+const fileName = "./examples/logo.svg";
 
-
-// questions for the creation of the new logo:
-const questions =  
-
-[
-
-
+const promptQuestions = [
   {
-  type: 'input',
-  name: 'Logo Name ',
-  message: 'Please enter text, cannot be more than 3 letters',    
-  validate: text => {
-     if (text.length > 3) {
-       return 'Text must not exceed 3 characters';
-     }
-     return true; 
-  } 
-
+    type: "list",
+    name: "shape",
+    message:
+      "What shape would you like to use as your logo? Please select ONE from the list.",
+    choices: ["Circle", "Triangle", "Square"],
   },
-
   {
-  type: 'input',
-  name: 'Text-Color',
-  message: 'Please enter a text color or hexadecimal:', // Be sure to allow keyword or hexadecimal number 
-  }, 
-
-  {
-   type: 'list',
-   name: 'shape',
-   message: 'Please select a shape',
-   choices: ['Circle, Triangle, Square',]
+    type: "input",
+    name: "text",
+    message: "Please choose up tp 3 letters to place on your logo.",
+    validate: (answer) => {
+      if (answer.length > 3) {
+        return console.log("Please choose up to 3 characters for your text!");
+      }
+      return true;
+    },
   },
-
   {
-   type: 'input',
-   name: 'shapeColor',
-   message:'Please select a color or enter a hexadecimal for the shape', // Be sure to allow keyword or hexadecimal number 
+    name: "shapeColorChoice",
+    message:
+      "Please choose a method to input your color choice in the next question.",
+    type: "list",
+    choices: ["color keyword", "hexadecimal"],
   },
-  
-]
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "What color would you like to use for the color of your shape?",
+    when: (answers) => {
+      if (answers.shapeColorChoice === "color keyword") {
+        return true;
+      }
+      return false;
+    },
+    validate: (answer) => {
+      let answerLowercase = answer.toLowerCase();
+      for (var i = 0; i < colorKeywordChoices.length; ++i) {
+        if (answerLowercase.indexOf(colorKeywordChoices[i]) != -1) {
+          return true;
+        }
+      }
+      return console.log("Please enter a valid color keyword.");
+    },
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message:
+      "What hexadecimal number (#CCCCCC) would you like to use for the color of your shape?",
+    when: (answers) => {
+      if (answers.shapeColorChoice === "hexadecimal") {
+        return true;
+      }
+      return false;
+    },
+    validate: (answer) => {
+      const hexRegEx = "^#[A-Fa-f0-9]{6}$";
+      if (!answer.match(hexRegEx)) {
+        return console.log(
+          "Please enter a valid hexadecimal number starting with '#' for the color of your shape."
+        );
+      }
+      return true;
+    },
+  },
+  {
+    name: "textColorChoice",
+    message:
+      "Please choose a method to input your color choice in the next question.",
+    type: "list",
+    choices: ["color keyword", "hexadecimal"],
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "What color would you like to use for the color of your text?",
+    when: (answers) => {
+      if (answers.textColorChoice === "color keyword") {
+        return true;
+      }
+      return false;
+    },
+    validate: (answer) => {
+      let answerLowercase = answer.toLowerCase();
+      for (var i = 0; i < colorKeywordChoices.length; ++i) {
+        if (answerLowercase.indexOf(colorKeywordChoices[i]) != -1) {
+          return true;
+        }
+      }
+      return console.log("Please enter a valid color keyword.");
+    },
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message:
+      "What hexadecimal number (#CCCCCC) would you like to use for the color of your text?",
+    when: (answers) => {
+      if (answers.textColorChoice === "hexadecimal") {
+        return true;
+      }
+      return false;
+    },
+    validate: (answer) => {
+      const hexRegEx = "^#[A-Fa-f0-9]{6}$";
+      if (!answer.match(hexRegEx)) {
+        return console.log(
+          "Please enter a valid hexadecimal number starting with '#' for the color of your text."
+        );
+      }
+      return true;
+    },
+  },
+];
 
-// Saves to SVG file 
-
-inquirer.prompt(questions).then(answers => {
-  const { text, textColor, shape, shapeColor } = answers;
-  const logo = generateLogo(text, textColor, shape, shapeColor);
-  fs.writeFile('./logo.svg', logo, err => {
-    if (err) {
-      console.log('Error writing file:', err);
-    } else {
-     console.log("Generated logo.svg");
-    }
-  });
-});
-
-//Generate logo:
-
-function generateLogo(text, textColor, shape, shapeColor) {
-    let shapeSvg;
-    switch(shape) {
-      case 'Circle':
-        shapeSvg = new Circle();
-        break;
-      case 'Square':
-        shapeSvg = new Square();
-        break;
-      case 'Triangle':
-        shapeSvg = new Triangle();  
-        break;
-    }
-  shapeSvg.colorChoice(shapeColor)
-  return `<svg width="300" height="200" xmlns="https://www.w3.org/2000/svg">
-   ${shapeSvg.render()}
-   <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-size="70">${text}</text>
-  
-  </svg>`;   
+async function createLogo(response) {
+  try {
+    const svg = await setShape(response);
+    fs.writeFile(fileName, svg, (err) => {
+      if (err) throw err;
+      console.log("Successfully generated logo.svg!");
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+async function init() {
+  try {
+    const response = await inquirer.prompt(promptQuestions);
+    await createLogo(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
